14d1284cedade78fd21fbbaed4ac8dcf65c9fcb6
lauraAlabau <arlaura3@gmail.com>
2021-08-30 19:33:18 +0200

    Home + About + (some) works

---
+++ app.js
@@ -0,0 +1,18 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+// ...
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// our first Route:
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+/* app.get('/gallery', (request, response, next) => response.sendFile(__dirname + '/views/gallery.html')); */
+
+// Server Started
+app.listen(3000, () => console.log('Comprobacion de que funciona '));
